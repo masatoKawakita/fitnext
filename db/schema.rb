420b5c94b6c96b1d7cb834ad0f96f40abd6d9cd6
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-08-14 17:11:26 +0900

    Update:Usersテーブルカラム

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,18 @@
-ActiveRecord::Schema.define(version: 2019_08_14_063935) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_08_14_074552) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
@@ -22,6 +35,16 @@ ActiveRecord::Schema.define(version: 2019_08_14_063935) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", limit: 100, null: false
+    t.date "birthday", null: false
+    t.integer "sex", null: false
+    t.string "tel", null: false
+    t.string "picture"
+    t.string "address"
+    t.text "infomation"
+    t.string "skype"
+    t.boolean "admin", default: false, null: false
+    t.integer "trainer_id", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
