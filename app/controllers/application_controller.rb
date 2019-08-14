725ea21384a974e61a38ec201b75a7af8ddcea53
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-08-14 17:53:37 +0900

    Update:application_controller編集　パラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     user_show_path
   end
@@ -6,5 +9,11 @@ class ApplicationController < ActionController::Base
 private
   def sign_in_required
     redirect_to homes_url unless user_signed_in?
+  end
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
