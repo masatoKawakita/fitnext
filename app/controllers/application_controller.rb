b21671511e14c7a3c5fa2d53d2465234afd0607c
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-08-16 15:09:09 +0900

    Update:usersコントローラー 他のユーザーページのURLに飛ぼうとするとアラートがでる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    edit_user_registration_path
-  end
-
   # def after_sign_in_path_for(resource)
-  #   flash[:notice] = "ログインしました"
-  #   # user_path(current_user.id)
-  #
+  #   edit_user_registration_path
   # end
+
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインしました"
+    user_path(current_user)
+  end
 
   # def after_sign_out_path_for(resource)
   #   flash[:alert] = "ログアウトしました"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,10 @@
 class UsersController < ApplicationController
-  before_action :sign_in_required, only: [:show]
-  before_action :set_user, only: [:show, :edit, :update]
 
-
-  def new
-    @user = User.new
-  end
-
-  def create
-  end
-
-  def update #TODO:アップデートしたら、編集画面へ
-  end
-
-  def set_user
+  def show
     @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to homes_show_path,  alert: 'URLが正しくありません。'
+    end
   end
+
 end
