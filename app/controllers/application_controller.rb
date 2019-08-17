b8ba16ec03cb8ded843fe391cc35ba3f712e9964
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-08-17 21:22:24 +0900

    Update:パスワード変更後、プロフィールページへ飛ぶ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #   edit_user_registration_path
-  # end
-
   def after_sign_in_path_for(resource)
     flash[:notice] = "ログインしました"
     # redirect_to user_path(current_user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,30 @@
+# Rails.application.routes.draw do
+#   root 'homes#index'
+#   get 'homes/show'
+#   # get 'users/:id' => 'users#show'
+
+#   devise_for :users
+#   resources :users
+
+#   devise_scope :user do
+#     get "sign_in", :to => "users/sessions#new"
+#     get "sign_out", :to => "users/sessions#destroy"
+#   end
+# end
+
 Rails.application.routes.draw do
   root 'homes#index'
   get 'homes/show'
   # get 'users/:id' => 'users#show'
 
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+  }
+
   resources :users
 
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy"
   end
-
 end
