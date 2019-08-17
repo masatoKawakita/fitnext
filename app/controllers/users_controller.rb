1e0872df00a9e2a2cb3e1f83f1f556d403dc6421
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-08-17 18:52:21 +0900

    Update:プロフィール編集を反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,39 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])
     if current_user != @user
       redirect_to homes_show_path,  alert: 'URLが正しくありません。'
     end
   end
 
+  def edit
+  end
+
   def update
     if @user.id == current_user.id
       if @user.update(user_params)
-        redirect_to user_path(@user.id), success: "情報を更新しました"
+        redirect_to user_path(@user.id), notice: "情報を更新しました"
       else
-        render "new", danger: "更新に失敗しました"
+        render "new", alert: "更新に失敗しました"
       end
+    end
+  end
+
+  def destroy
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :birthday, :sex, :email, :tel, :picture, :address,
+                                  :infomation, :skype, :encrypted_password, :picture_cache, :remove_picture)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+    if current_user != @user
+        redirect_to user_path,  alert: '違反行為です! 他のユーザーページにはいけません。'
     end
   end
 end
