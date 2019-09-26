36ae72850f178fa3afd6345c7b7fd129400fcdd8
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-09-26 15:14:50 +0900

    Update:DM用userコントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,25 @@ before_action :authenticate_user!, only: [:edit, :update, :destroy, :new, :creat
       @user = User.find_by(id: params[:id])
       @trainer = Trainer.find_by(user_id: @user.id)
     end
+
+    if @user.id == current_user.id
+      @user = User.find_by(id: params[:id])
+      @trainer = Trainer.find_by(user_id: @user.id)
+      @currentUserEntry=Entry.where(user_id: current_user.id)
+      @userEntry=Entry.where(user_id: @user.id)
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
   end
 
   def edit
