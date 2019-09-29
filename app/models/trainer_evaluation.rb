0e336aa08c1f1e7f23a5105d5d407eeb8199aa93
MasatoKawakita <masatoKawakita@users.noreply.github.com>
2019-09-29 17:54:48 +0900

    fix filename unique

--- app/models/trainer_evaluation.rb
+++ app/models/trainer_evaluation.rb
@@ -1,4 +1,15 @@
 class TrainerEvaluation < ApplicationRecord
   belongs_to :user
   belongs_to :trainer
+
+  before_save :check_rate
+  before_update :check_rate
+
+  def check_rate
+    self.fun_rate ||= 0
+    self.understandable_rate ||= 0
+    self.politeness_rate ||= 0
+    self.kindness_rate ||= 0
+    self.passion_rate ||= 0
+  end
 end
